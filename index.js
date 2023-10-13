a1e3e46add6b18f3eb54b77723bfb78c045bcac0
DCKAP <varshithaksdckap@gmail.com>
2023-10-13 14:46:36 +0530

    switch

---
+++ index.js
@@ -0,0 +1,24 @@
+let college=prompt("Choose any Semester from 1st semester to 6th semester");
+switch(college)
+{
+    case "1st semester" :
+        console.log("First semester went in lockdown and the classes were attended in online");
+        break;
+    case "2nd semester" :
+        console.log("Only one month went to college and enjoyed with friends");
+        break;
+    case "3rd semester" :
+        console.log("Third semester went like first semester");
+        break;
+    case "4th semester" :
+        console.log("Wrote my first offline semester");
+        break;
+    case "5th semester" :
+        console.log("College teacher were changed");
+        break;
+    case "6th semester" :
+        console.log("I lost  one of my friend");
+        break;
+    default :
+        console.log("Now i am software trainee in DCKap Palli");
+}
